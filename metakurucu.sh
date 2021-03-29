e2e27fad49519bf2f76b4f361978f2ec4c665323
jokzilla <66950270+jokzilla@users.noreply.github.com>
2021-03-29 18:22:41 +0300

    Create metakurucu.sh

---
+++ metakurucu.sh
@@ -0,0 +1,328 @@
+#!/bin/bash 
+echo -e '\033[33;35m=============================================================\e[0m
+\e[34m=\e[0m\e[1;31;42m  ▄▀▀▄ ▄▀▄  ▄▀▀▄▀▀▀▄  ▄▀▀▄▀▀▀▄  ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀▀█▀▀▄ \e[0m\e[34m=\e[0m
+\e[34m=\e[0m\e[1;31;42m █  █ ▀  █ █   █   █ █   █   █ █      █ █      █ █    █  ▐ \e[0m\e[34m=\e[0m
+\e[34m=\e[0m\e[1;31;42m ▐  █    █ ▐  █▀▀█▀  ▐  █▀▀█▀  █      █ █      █ ▐   █     \e[0m\e[34m=\e[0m
+\e[34m=\e[0m\e[1;31;42m   █    █   ▄▀    █   ▄▀    █  ▀▄    ▄▀ ▀▄    ▄▀    █      \e[0m\e[34m=\e[0m
+\e[34m=\e[0m\e[1;31;42m ▄▀   ▄▀   █     █ ▄ █     █     ▀▀▀▀     ▀▀▀▀    ▄▀       \e[0m\e[34m=\e[0m
+\e[34m=\e[0m\e[1;31;42m █    █    ▐     ▐   ▐     ▐                     █         \e[0m\e[34m=\e[0m
+\e[34m=\e[0m\e[1;31;42m ▐    ▐                                          ▐         \e[0m\e[34m=\e[0m
+\033[33;35m=============================================================\e[0m
+\e[5;30;48;5;82mINSTAGRAM---->atess681\e[0m'
+echo
+echo -e "\e[30;48;5;82m[1]\e[0m \e[30;48;5;82mKURULMADAN ÖNCE BİLİNMESİ GEREKENLER\e[0m"
+echo -e "\e[30;48;5;82m[2]\e[0m \e[30;48;5;82mMETASPLOİT-FRAMEWORK KURULUM\e[0m"
+echo -e "\e[30;48;5;82m[3]\e[0m \e[30;48;5;82mÇIKIŞ\e[0m"
+echo
+read -e -p $'\e[31mroot@kali~#\e[0m'  secim
+clear
+if [[ $secim == 1 ]];then
+	echo -e '
+\e[1;31;42mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNddNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMN+````:yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMh.       -hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMy`         `/NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMd`           `.hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMM/              `/NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMh                `-hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMm-                  `oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMN+/oyhhdddhys+/-`      -omMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMNNMMMMMMMMMMMMMMNds:`  `+:-+shdNMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm+`.h/`    `-omNMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNy`       ``sMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs`  `..``+ `sMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMMMMMMMMMm.  `/N+ +d  .mMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMd./NMMMMMMMMMMMMMy`  -NN.`mN`  yMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMNm`/MMMMMMMMMMMMMM/   +My oMN:  /MMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMNy:dMMMMMMMMMMMMMM-  `hN--NMMs  `NMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMN:mNMMMMMMMMMMMMMN.  :Nd`yMMMm` `sMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMm.mMMMMMMMMMMMMMMm``/NMy-NMMMN-`+-NMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMy+mMMMMMMMMMMMMMMd`:NMMMMMMMMN--NymMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMN+NMMMMMMMMMMMMMMMy`dMMMMMMMMMm.hMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMmmMMMMMMMMMMMMMMMNy/NMMMMMMMMm:sMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMyyMMMMMMMMMMMMMMMm-dMMMhsMMMh-yNMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMy+MMMMMMMMMMMMMMMmyMMMMm/NNy:dMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMNmNMMMMMMMMMMMMMNMMMMMMMMMh/y/dMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMyoNMMMMMMMMMMMMNyhNMMMMMMMN/:hMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMm:hNMMMMMMMMMMMMNsdmMMMMMm+ohNMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:s/NMMMMMNs/sdNNMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN+.sdMMMMMN/:yho+hNMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo+NNMMMMNy/` .yNMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN++NMMMMNo-`   `oNMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm+yNMMMMMh/Nm++o:+NMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMdNMMMMMMdh+/sNdhNmymMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNh`-NNmhys:hNMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd.oNMMMMNNmNMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\e[0m
+\e[5;30;48;5;82mINSTAGRAM---->atess681\e[0m'
+	
+	echo -e "\e[43mMetasploiti kurmadan önce termuxu sıfırlayın\e[0m"
+	echo -e "\e[43mSıfırladıktan sonra git paketi dışında hiç bir paket indirmeyiniz yoksa\e[0m"
+	echo -e "\e[43mÇalışmaz terkrar ruby hatası alırsınız\e[0m"
+	echo -e "\e[43mKurulum bittikten sonra metasploit dosyasını cd diyerek seçiyoruz\e[0m"
+	echo -e "\e[43mArdından ./msfconsole yazıyoruz artık çalışıyor :)\e[0m"
+	echo -e "\e[43mGelen sorularda y basın\e[0m"
+	echo -e "\e[43mMetasploit-Framework hakkında daha çok bilgi için buraya bakın\e[0m"
+	echo -e "\e[43mhttps://www.turkhackteam.org/linux/1316435-m-e-t-s-p-l-o-i-t-kullanimi-shujaira.html\e[0m"
+	
+	
+elif [[ $secim == 2 ]];then
+	echo -e "\e[42m... [H]=========Mr.Root========[-][o][x]\e[0m
+\e[42m... |      \e[30;48;5;82mMetasploit Framework\e[0m\e[42m        |\e[0m
+\e[42m... |           \e[30;48;5;82mRuby Eror\e[0m\e[42m              |\e[0m
+\e[42m... |             \e[30;48;5;82mFİX\e[0m\e[42m                  |\e[0m
+\e[42m... |           \e[30;48;5;82mSürüm 1.0\e[0m\e[42m              |\e[0m
+\e[42m... |================================= |\e[0m\e[42m\e[0m
+    	"
+    	sleep 1
+	echo -e '\e[34mBlue
+            ######
+            ##
+            #####
+                ##
+                ##
+            ##  ##
+             ####
+	\e[0m'  
+	echo -e '\e[5;32m
+
+
+            \e[32m                             \e[31m .___.
+            \e[32m          /)              \e[31m ,-^     ^-.
+            \e[32m         //             \e[31m  /           /
+            \e[32m--------| |--------------\e[31m/  __     __  /\e[32m-------------------___
+            \e[32m|WMWMWMW| |>>>>>>>>>>>>>\e[31m | />>\   />>\ |\e[32m>>>>>>>>>>>>>>>>>>>>>>:>
+            \e[32m--------| |--------------\e[31m| \__/   \__/ |\e[32m-------------------^^
+            \e[32m         //               \e[31m\    /|\    /
+            \e[32m 	      \)               \e[31m\   \_/   /
+            \e[32m                            \e[31m|       |
+            \e[32m                            \e[31m|+H+H+H+|
+            \e[32m                           \e[31m \       /
+            \e[32m                             \e[31m^-----^
+
+
+                 _     _ _______ _______ _     _ _____ __   _  ______
+                 |_____| |_____| |       |____/    |   | /  | |  ____
+                 |     | |     | |_____  |    /_ __|__ |  /_| |_____|
+
+
+                     \e[5;36m  《《《《《《《《 Mr.Root 》》》》》》》》\e[0m'
+	sleep 1
+	echo -e '\e[35m
+               ###
+              ####
+             ## ##
+            ##  ##
+            #######
+                ##
+                ##	\e[m0'
+	echo -e '
+	
+\e[5;33m                              _________-----_____
+                   _____------           __      ----_
+            ___----             ___------              \
+               ----________        ----                 \
+                           -----__    |             _____)
+                                __-                /     \
+                    _______-----    ___--          \    /)\
+              ------_______      ---____            \__/  /
+                           -----__    \ --    _          /\
+                                  --__--__     \_____/   \_/\
+                                          ----|   /          |
+                                              |  |___________|
+                                              |  | ((_(_)| )_)
+                                              |  \_((_(_)|/(_)
+                                              \             (
+                                               \_____________)\e[0m
+	'
+	sleep 1
+	echo -e '\e[36m
+              ###
+            ##  ##
+                ##
+              ###
+                ##
+            ##  ##
+             ####\e[0m	
+	'
+	echo -e '\e[5;92m
+                             uuuuuuu
+                         uu$$$$$$$$$$$uu
+                      uu$$$$$$$$$$$$$$$$$uu
+                     u$$$$$$$$$$$$$$$$$$$$$u
+                    u$$$$$$$$$$$$$$$$$$$$$$$u
+                   u$$$$$$$$$$$$$$$$$$$$$$$$$u
+                   u$$$$$$$$$$$$$$$$$$$$$$$$$u
+                   u$$$$$$"   "$$$"   "$$$$$$u
+                   "$$$$"      u$u       $$$$"
+                    $$$u       u$u       u$$$
+                    $$$u      u$$$u      u$$$
+                     "$$$$uu$$$   $$$uu$$$$"
+                      "$$$$$$$"   "$$$$$$$"
+                        u$$$$$$$u$$$$$$$u
+                         u$"$"$"$"$"$"$u
+              uuu        $$u$ $ $ $ $u$$       uuu
+             u$$$$        $$$$$u$u$u$$$       u$$$$
+              $$$$$uu      "$$$$$$$$$"     uu$$$$$$
+            u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$
+            $$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"
+             """      ""$$$$$$$$$$$uu ""$"""
+                       uuuu ""$$$$$$$$$$uuu
+              u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$
+              $$$$$$$$$$""""           ""$$$$$$$$$$$"
+               "$$$$$"                      ""$$$$""
+                 $$$"                         $$$$"\e[0m
+	'
+	sleep 1
+	echo -e '
+             \e[30;48;5;82m####\e[0m
+            \e[30;48;5;82m##\e[30;48;5;82m  \e[0m##\e[0m
+                \e[30;48;5;82m##\e[0m
+              \e[30;48;5;82m###\e[0m
+             \e[30;48;5;82m##\e[0m
+            \e[30;48;5;82m##\e[30;48;5;82m  \e[0m##\e[0m
+            \e[30;48;5;82m######\e[0m
+	'      
+	echo -e "\e[5;93m
+	
+                            ..:::::::::..
+                       ..:::aad8888888baa:::..
+                    .::::d:?88888888888?::8b::::.
+                  .:::d8888:?88888888??a888888b:::.
+                .:::d8888888a8888888aa8888888888b:::.
+               ::::dP::::::::88888888888::::::::Yb::::
+              ::::dP:::::::::Y888888888P:::::::::Yb::::
+             ::::d8:::::::::::Y8888888P:::::::::::8b::::
+            .::::88::::::::::::Y88888P::::::::::::88::::.
+            :::::Y8baaaaaaaaaa88P:T:Y88aaaaaaaaaad8P:::::
+            :::::::Y88888888888P::|::Y88888888888P:::::::
+            ::::::::::::::::888:::|:::888::::::::::::::::
+             :::::::::::::::8888888888888b::::::::::::::
+             :::::::::::::::88888888888888::::::::::::::
+              :::::::::::::d88888888888888:::::::::::::
+               ::::::::::::88::88::88:::88::::::::::::
+                 ::::::::::88::88::88:::88::::::::::'
+                   ::::::::88::88::P::::88::::::::'
+                     ::::::88::88:::::::88::::::
+                         :::::::::::::::::::
+                              :::::::::\e[0m
+	"   
+	sleep 1
+	echo -e '\e[40;38;5;82m
+              ##
+             ###
+              ##
+              ##
+              ##
+              ##
+            ######\e[0m	
+	'        
+	echo -e '\e[5;96m
+	
+                                  :::!~!!!!!:.
+                              .xUHWH!! !!?M88WHX:.
+                            .X*#M@$!!  !X!M$$$$$$WWx:.
+                           :!!!!!!?H! :!$!$$$$$$$$$$8X:
+                          !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+                         :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+                         ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+                           !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+                           ~?WuxiW*`   `"#$$$$8!!!!??!!!
+                         :X- M$$$$       `"T#$T~!8$WUXU~
+                        :%`  ~#$$$m:        ~!~ ?$$$$$$
+                      :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+            .....   -~~:<` !    ~?T#$$@@W@*?$$      /`
+            W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+            #"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+            :::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+            .~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+            Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+            $R@i.~~ !     :   ~$$$$$B$$en:``
+            ?MXT@Wx.~    :     ~"##*$$$$M~\e[0m	
+
+	'
+	sleep 1
+	echo -e '
+            \e[40;38;5;82m░█░█░█░█░█▀▄░█░█░█░░░█░█░█▄█░\e[0m\e[40;38;5;82m░░█▀▄░█▀█░█▀▀░█░░░█▀█░█▀▄░▀█▀\e[0m
+            \e[40;38;5;82m░█▀▄░█░█░█▀▄░█░█░█░░░█░█░█░█░\e[0m\e[40;38;5;82m░░█▀▄░█▀█░▀▀█░█░░░█▀█░█░█░░█░\e[0m
+            \e[40;38;5;82m░▀░▀░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░\e[0m\e[40;38;5;82m░░▀▀░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀░░▀▀▀\e[0m
+	'
+	sleep 5
+        termux-setup-storage
+	pkg install postgresql
+	pkg install wget
+	pkg install python
+	pkg install python2
+	wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh
+	bash install.sh -i
+	pkg install ruby2
+	pkg install ruby
+	wget https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+	chmod +x metasploit.sh
+	./metasploit.sh
+	pg_ctl -D /data/data/com.termux/files/usr/var/lib/postgresql -l logfile start
+	cd metasploit-framework
+	./msfconsole
+	exit
+	cd ..
+	rm -rf METASPLOİT.sh
+	
+elif [[ $secim == 3 ]];then
+	echo -e '\e[96m
+	
+                       uuuuuuuuuuuuuuuuuuuuu.
+                   .u$$$$$$$$$$$$$$$$$$$$$$$$$$W.
+                 u$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$Wu.
+               $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$i
+              $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+         `    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+           .i$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$i
+           $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$W
+          .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$W
+         .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$i
+         #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$.
+         W$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$u       #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$~
+$#      `"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$i        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$        #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$         $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#$.        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+ $$      $iW$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$!
+ $$i      $$$$$$$#"" `"""#$$$$$$$$$$$$$$$$$#""""""#$$$$$$$$$$$$$$$W
+ #$$W    `$$$#"            "       !$$$$$`           `"#$$$$$$$$$$#
+  $$$     ``                 ! !iuW$$$$$                 #$$$$$$$#
+  #$$    $u                  $   $$$$$$$                  $$$$$$$~
+   "#    #$$i.               #   $$$$$$$.                 `$$$$$$
+          $$$$$i.                """#$$$$i.               .$$$$#
+          $$$$$$$$!         .   `    $$$$$$$$$i           $$$$$
+          `$$$$$  $iWW   .uW`        #$$$$$$$$$W.       .$$$$$$#
+            "#$$$$$$$$$$$$#`          $$$$$$$$$$$iWiuuuW$$$$$$$$W
+               !#""    ""             `$$$$$$$##$$$$$$$$$$$$$$$$
+          i$$$$    .                   !$$$$$$ .$$$$$$$$$$$$$$$#
+         $$$$$$$$$$`                    $$$$$$$$$Wi$$$$$$#"#$$`
+         #$$$$$$$$$W.                   $$$$$$$$$$$#   ``
+          `$$$$##$$$$!       i$u.  $. .i$$$$$$$$$#""
+             "     `#W       $$$$$$$$$$$$$$$$$$$`      u$#
+                            W$$$$$$$$$$$$$$$$$$      $$$$W
+                            $$`!$$$##$$$$``$$$$      $$$$!
+                           i$" $$$$  $$#"`  """     W$$$$
+                                                   W$$$$!
+                      uW$$  uu  uu.  $$$  $$$Wu#   $$$$$$
+                     ~$$$$iu$$iu$$$uW$$! $$$$$$i .W$$$$$$
+             ..  !   "#$$$$$$$$$$##$$$$$$$$$$$$$$$$$$$$#"
+             $$W  $     "#$$$$$$$iW$$$$$$$$$$$$$$$$$$$$$W
+             $#`   `       ""#$$$$$$$$$$$$$$$$$$$$$$$$$$$
+                              !$$$$$$$$$$$$$$$$$$$$$#`
+                              $$$$$$$$$$$$$$$$$$$$$$!
+                            $$$$$$$$$$$$$$$$$$$$$$$`
+                             $$$$$$$$$$$$$$$$$$$$"                 ^    ^      \^ ^           ^\/^     ^            ^    ^      \^ ^           ^\/^     ^
+	\e[0m'
+	echo -e "Tekrar Bekleriz"
+	exit
+else
+	echo "hatalı seçim"
+fi
